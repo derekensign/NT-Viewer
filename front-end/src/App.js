19f3ee497a1dfad6519ac0b46ff0552190bedb1a
Derek Ensign <derekensign@gmail.com>
2021-10-11 14:20:53 -0500

    added box header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
@@ -6,7 +7,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <Box
+        sx={{
+          width: 1,
+          height: 200,
+          bgcolor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      />
     </div>
 
   );
